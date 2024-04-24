22856bd503b629cd5fc26cb8bf2e470ec97f510f
Roman <rdyakovich@gmail.com>
2024-04-24 17:27:25 +0300

    test

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.scss';
+//import './styles/main.scss';
+import { Navigation } from './components/Navigation';
+import { TradeDesign } from './components/TradeDesign';
+
+function App() {
+  return (
+    <div className="main">
+      <Navigation />
+      {/*<TradeDesign />*/}
+    </div>
+  );
+}
+export default App;
